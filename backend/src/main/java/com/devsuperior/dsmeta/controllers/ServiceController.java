cc7febe4d4b81e06694a2822d87666b7305a3e2f
r-avellar <renatoavellar5@gmail.com>
2022-11-16 14:24:26 -0300

    Twilio sms

--- backend/src/main/java/com/devsuperior/dsmeta/controllers/ServiceController.java
+++ backend/src/main/java/com/devsuperior/dsmeta/controllers/ServiceController.java
@@ -1,15 +1,12 @@
 package com.devsuperior.dsmeta.controllers;
 
 import com.devsuperior.dsmeta.entities.Sale;
+import com.devsuperior.dsmeta.services.SMSService;
 import com.devsuperior.dsmeta.services.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,12 +14,18 @@ import java.util.List;
 @RequestMapping(value = "/sales")
 public class ServiceController {
     @Autowired
-    private SaleService service;
+    private SaleService saleService;
 
+    @Autowired
+    private SMSService smsService;
     @GetMapping
     public Page<Sale> findSales( @RequestParam(value = "minDate", defaultValue = "") String minDate,
                                  @RequestParam(value = "maxDate", defaultValue = "")String maxDate,Pageable pageable){
-        return service.findSales(minDate, maxDate, pageable);
+        return saleService.findSales(minDate, maxDate, pageable);
+    }
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
     }
 }
 
